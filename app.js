f0195ee542e8a26a37fe8147a05b0b6a895b3407
cleverAkanimoh <crushclever1@gmail.com>
2022-12-24 11:33:43 +0100

    modified app.js in .

--- app.js
+++ app.js
@@ -6,11 +6,6 @@ const container = document.querySelector('#container');
 
 const input = document.querySelector('#urlField');
 
-const userID = input.value;
-const key = 'AIzaSyD7DgWzi2Whte2Wz447C4stmSrKmfE7ZtI';
-
-const url = `https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${userID}&key=${key}`;
-
 // display alerts
 const displayAlert = (txt, dur) => {
     alert.innerText = txt;
@@ -24,56 +19,75 @@ const displayAlert = (txt, dur) => {
 // find data for search
 const findInfo = async e => {
 
+    const userID = input.value;
+    const key = 'AIzaSyD7DgWzi2Whte2Wz447C4stmSrKmfE7ZtI';
+
+    const url = `https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${userID}&key=${key}`;
+
     e.preventDefault();
+
+    console.log(userID);
 
     container.innerHTML = `
         <div id="loading"></div>
 		<p id="loadtext">finding result...</p>
     `;
-    try {
-        const response = await fetch(url, {
-            headers: {
-                Accept: 'application/json',
-                'User-Agent': 'learning app',
-            },
-        });
-        if (!response.ok) throw new Error(' error');
-        
-        const data = await response.json();
-        console.log(data);
-        
-        if (data.pageIndo.totalResults > 0) {
-            let title = 'cleverly';
-            let subscriberCount = data.items[0].statistics.sunscriberCount;
-            let videoCount = data.items[0].statistics.videoCount;
-            let viewsCount = data.items[0].statistics.viewsCount;
+    // try {
+    //     const response = await fetch(url, {
+    //         headers: {
+    //             Accept: 'application/json',
+    //             'User-Agent': 'learning app',
+    //         },
+    //     });
+    //     if (!response.ok) {
+    //         throw new Error(' error')
+    //     };
 
-            container.innerHTML = `
-                <div id="title">
-			    	<h2>${title}</h2>
-			    </div>
+    //     const data = await response.json();
+    //     console.log(data);
 
-			    <aside>
-			    	<div id="subscribers">
-			    		<button>subscribers</button>
-			    		<h3>${subscriberCount}</h3>
-			    	</div>
+    //     // if (data.pageIndo.totalResults > 0) {
+    //     //     let title = 'cleverly';
+    //     //     let subscriberCount = data.items[0].statistics.sunscriberCount;
+    //     //     let videoCount = data.items[0].statistics.videoCount;
+    //     //     let viewsCount = data.items[0].statistics.viewsCount;
 
-				<div id="views">
-					<button>views</button>
-					<h3>${videoCount}</h3>
-				</div>
+    //     //     container.innerHTML = `
+    //     //         <div id="title">
+    // 	// 	    	<h2>${title}</h2>
+    // 	// 	    </div>
 
-				<div id="videos">
-					<button>total videos</button>
-					<h3>${viewsCount}</h3>
-				</div>
-			</aside>
-        `;
+    // 	// 	    <aside>
+    // 	// 	    	<div id="subscribers">
+    // 	// 	    		<button>subscribers</button>
+    // 	// 	    		<h3>${subscriberCount}</h3>
+    // 	// 	    	</div>
+
+    // 	// 		<div id="views">
+    // 	// 			<button>views</button>
+    // 	// 			<h3>${videoCount}</h3>
+    // 	// 		</div>
+
+    // 	// 		<div id="videos">
+    // 	// 			<button>total videos</button>
+    // 	// 			<h3>${viewsCount}</h3>
+    // 	// 		</div>
+    // 	// 	</aside>
+    //     // `;
+    //     // }
+    // } catch (error) {
+    //     container.textContent = `${error} Also ensure you have a good internet connection`;
+    // };
+
+    fetch(url, {
+        headers: {
+            Accept: 'application/json',
+            'User-agent': 'youtubeProject'
         }
-    } catch (error) {
-        container.textContent = `${error} Also ensure you have a good internet connection`;
-    };
+    })
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(err => console.log(err))
 };
 
 findBtn.onclick = findInfo;
